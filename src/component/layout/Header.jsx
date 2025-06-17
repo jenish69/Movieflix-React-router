7a7b68713cbad768ce8a1d6978aed4937bdf78c1
Jenish Patel <84135743+jenish69@users.noreply.github.com>
2025-06-17 12:10:53 +0530

    Update Header.jsx

--- src/component/layout/Header.jsx
+++ src/component/layout/Header.jsx
@@ -6,18 +6,6 @@ export const Header = () => {
   return (
     <>
       <header className="section-navbar">
-        <section className="top_txt">
-          <div className="head container">
-            <div className="head_txt">
-              <p>Get Thapa Membership, 30-day return or refund guarantee.</p>
-            </div>
-            <div className="sing_in_up">
-              <NavLink to="# ">SIGN IN</NavLink>
-              <NavLink to="# ">SIGN UP</NavLink>
-            </div>
-          </div>
-        </section>
-
         <div className="container">
           <div className="navbar-brand">
             <NavLink to="index">
